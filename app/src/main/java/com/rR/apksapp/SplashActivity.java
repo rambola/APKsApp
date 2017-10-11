c71417dd3ce0aa6da0b3a0c3249f007599cba35a
rambola <racha4rambola@gmail.com>
2017-10-11 19:11:50 +0530

    Initial Commit

---
+++ app/src/main/java/com/rR/apksapp/SplashActivity.java
@@ -0,0 +1,53 @@
+package com.rR.apksapp;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+    Handler handler;
+    Runnable runnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, Dashboard.class));
+                SplashActivity.this.finish();
+            }
+        };
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        handler.postDelayed(runnable, 1500);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        handler.removeCallbacks(runnable);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        if (null != handler)
+            handler = null;
+
+        if (null != runnable)
+            runnable = null;
+    }
+
+}
---
+++ app/src/main/java/com/rR/apksapp/adapters/DashboardAdapter.java
@@ -0,0 +1,156 @@
+package com.rR.apksapp.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.rR.apksapp.Dashboard;
+import com.rR.apksapp.R;
+import com.rR.apksapp.models.AppsApkDetails;
+import com.rR.apksapp.utils.GlideCircleTransform;
+
+import java.util.ArrayList;
+
+public class DashboardAdapter extends RecyclerView.Adapter<DashboardAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<AppsApkDetails> appsApkDetails;
+    private boolean isLongClicked = false;
+    private final String TAG = DashboardAdapter.class.getName();
+
+    public DashboardAdapter(Context context, ArrayList<AppsApkDetails> appsApkDetails) {
+        this.context = context;
+        this.appsApkDetails = appsApkDetails;
+    }
+
+    public void updateAdapter(Context context, ArrayList<AppsApkDetails> appsApkDetails) {
+        this.context = context;
+        this.appsApkDetails = appsApkDetails;
+
+        notifyDataSetChanged();
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(parent.getContext()).
+                inflate(R.layout.for_dashboard_grid, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder viewHolder, int position) {
+        viewHolder.bindData(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return appsApkDetails.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder implements
+            View.OnClickListener, View.OnLongClickListener {
+        CardView cardView;
+        TextView appNameTV;
+        ImageView appIconIV;
+        int clickedPosition;
+
+        private ViewHolder(View itemView) {
+            super(itemView);
+
+            cardView = itemView.findViewById(R.id.cardView);
+            appNameTV = itemView.findViewById(R.id.appNameTV);
+            appIconIV = itemView.findViewById(R.id.appIconIV);
+        }
+
+        public void bindData(int position) {
+            this.clickedPosition = position;
+            Glide.with(context).load("").placeholder(appsApkDetails.get(position)
+                    .getDrawable()).transform(new GlideCircleTransform(context)).
+                    into(appIconIV);
+            appNameTV.setText(appsApkDetails.get(position).getAppName());
+
+            if (appsApkDetails.get(position).getIsSelected()) {
+                itemView.setBackgroundColor(context.getResources().
+                        getColor(R.color.cardSelected));
+            } else {
+                itemView.setBackgroundColor(context.getResources().
+                        getColor(R.color.cardUnSelected));
+            }
+
+            itemView.setOnClickListener(this);
+            itemView.setOnLongClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            Log.d(TAG, "onClick(), clickedPosition: " + clickedPosition +
+                    ", isLongClicked: " + isLongClicked);
+
+            if (isLongClicked) {
+                if (appsApkDetails.get(clickedPosition).getIsSelected()) {
+                    appsApkDetails.get(clickedPosition).setIsSelected(false);
+                    itemView.setBackgroundColor(context.getResources().
+                            getColor(R.color.cardUnSelected));
+                } else {
+                    appsApkDetails.get(clickedPosition).setIsSelected(true);
+                    itemView.setBackgroundColor(context.getResources().
+                            getColor(R.color.cardSelected));
+                }
+            }
+
+            if (0 == getSelectedItemsCount()) {
+                isLongClicked = false;
+                ((Dashboard) context).showOrHideBottomBar(false);
+            }
+        }
+
+        @Override
+        public boolean onLongClick(View view) {
+            Log.d(TAG, "onLongClick(), clickedPosition: " + clickedPosition +
+                    ", isLongClicked: " + isLongClicked);
+
+            if (!isLongClicked) {
+                isLongClicked = true;
+                ((Dashboard) context).showOrHideBottomBar(true);
+
+                appsApkDetails.get(clickedPosition).setIsSelected(true);
+                itemView.setBackgroundColor(context.getResources().
+                        getColor(R.color.cardSelected));
+            }
+
+            return true;
+        }
+    }
+
+    private int getSelectedItemsCount() {
+        int selectedItemsCount = 0;
+        for (int i = 0; i < appsApkDetails.size(); i++) {
+            if (appsApkDetails.get(i).getIsSelected()) {
+                selectedItemsCount++;
+            }
+        }
+
+        Log.d(TAG, "getSelectedItemsCount(), selectedItemsCount: " + selectedItemsCount);
+        return selectedItemsCount;
+    }
+
+    public ArrayList<AppsApkDetails> getTheSelectedAppsList() {
+        ArrayList<AppsApkDetails> arrayList = new ArrayList<>();
+        arrayList.addAll(appsApkDetails);
+
+        for (int i = 0; i < arrayList.size(); i++) {
+            if (!arrayList.get(i).getIsSelected()) {
+                arrayList.remove(i);
+                i--;
+            }
+        }
+
+        return arrayList;
+    }
+
+}
